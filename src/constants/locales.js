315e43f138e44eeb658442179e3cadbc01b8ae9b
Vladimir Drannikov <krowen@inbox.ru>
2021-09-15 17:13:04 +0600

    Added constants

---
+++ src/constants/locales.js
@@ -0,0 +1,29 @@
+export default {
+  title: 'API-консолька',
+  formLogin: {
+    login: 'Логин',
+    sublogin: 'Сублогин',
+    password: 'Пароль',
+    label: 'Опционально',
+    button: 'Войти',
+    error: 'Вход не выполнен',
+  },
+  console: {
+    buttonExit: 'Выйти',
+    emptyList: 'Список запросов пуст',
+    labelRequest: 'Запрос',
+    labelResponse: 'Ответ',
+    buttonSubmit: 'Отправить',
+    buttonFormatting: 'Форматировать',
+    buttonFullscreen: 'На весь экран',
+    buttonReset: 'Удалить все запросы',
+    githubLink: '@link-to-my-github',
+  },
+  dropdown: {
+    copy: 'Копировать',
+    remove: 'Удалить',
+    execute: 'Выполнить',
+    feedbackCoping: 'Скопировано',
+    button: 'Открыть/закрыть меню',
+  },
+};
